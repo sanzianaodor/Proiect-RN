5305173ecff501571f3b2d6855ccd8b8dc615d7a
sanzianaodor <167864750+sanzianaodor@users.noreply.github.com>
2026-02-11 10:52:05 +0200

    Update model.py

--- Clasificarea_instrumentelor_muzicale/src/neural_network/model.py
+++ Clasificarea_instrumentelor_muzicale/src/neural_network/model.py
@@ -1,6 +1,5 @@
 import os
 import tensorflow as tf
-# Folosim importul standard. Chiar daca apare cu galben in VS Code, ESTE CORECT.
 from tensorflow.keras import layers, models # type: ignore
 import sys
 
@@ -22,8 +21,7 @@ def build_cnn_model(input_shape=INPUT_SHAPE, num_classes=NUM_CLASSES):
         # --- Blocul 1 ---
         layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape, padding='same'),
         layers.MaxPooling2D((2, 2)),
-        layers.BatchNormalization(), # Acum va functiona corect
-
+        layers.BatchNormalization(), 
         # --- Blocul 2 ---
         layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
         layers.MaxPooling2D((2, 2)),
@@ -78,7 +76,7 @@ if __name__ == "__main__":
     # 2. Afisam rezumatul
     model.summary()
     
-    # 3. Testam salvarea (cu numele corect untrained_model.h5)
+    # 3. Testam salvarea 
     save_model_skeleton(model)
     
     # 4. Testam reincarcarea
